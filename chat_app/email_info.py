4f563750d8b51bb41506082bfeeb9d47d20cec42
Mridul Gupta <47922198+Mrd278@users.noreply.github.com>
2019-09-03 11:46:09 +0530

    Update email_info.py

--- chat_app/email_info.py
+++ chat_app/email_info.py
@@ -1,6 +1,6 @@
 #for gmail or google apps
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='med6hmaasm@gmail.com'
-EMAIL_HOST_PASSWORD='hmaasm12345'
+EMAIL_HOST_USER='youremail@gmail.com'
+EMAIL_HOST_PASSWORD='yourpassword'
 EMAIL_PORT=587
